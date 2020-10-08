feffacdd24b91cf47b7b058c6b614ddb27850480
Nico Rithner <nicorithner@gmail.com>
2020-10-07 21:24:43 -0600

    Create Welcome page spec features test
    Laid out the scenarios (using describe and it) to
    meet the first story requirements

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe "As a visitor" do
+  describe "Home page features" do
+
+    before :each do
+      visit "/"
+    end
+
+    it "There is a Welcome message" do
+      expect(page).to have_css('h1', text: "Simple GIF Search App")
+      expect(page).to have_content("Back-end Development Student Project")
+    end
+
+    it "There instructions" do
+      within('.front_page_form') do
+        expect(page).to have_css('h2', text: "How To Use This App")
+        expect(page).to have_content("Enter your gif search in the form below.")
+      end
+    end
+
+    describe "There is a search form and a query redirects to 'search/index'" do
+      
+      it "There is a search form" do
+        within('.front_page_form') do
+          find_field('Search')
+          find_button('Submit')
+        end
+      end
+      
+      it "After adding a query and clicking 'Submit' visitor is redirected to 'search/index' page" do
+        within('.front_page_form') do
+          fill_in 'Search', with: 'Duffy Duck'
+          click_button('Submit')
+        end
+        expect(current_path).to eq('/search/index')
+      end
+
+    end
+  end
+end

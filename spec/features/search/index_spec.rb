e4d2c242c527d0ad314d0991d03f744354f2afce
Nico Rithner <nicorithner@gmail.com>
2020-10-09 15:09:55 -0600

    Create search index features tests

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor', type: :feature do
+  describe 'Search index page features' do
+    it "Sees the search words in the title and 10 pictures in the page matching the query" do
+      visit '/'
+      within('.front_page_form') do
+          fill_in 'Search for:', with: 'Duffy Duck'
+          click_button('Submit')
+        end
+        expect(current_path).to eq('/search/index')
+
+        expect(page).to have_content('Duffy Duck')
+
+        expect(page).to have_css(".gif", count: 10)
+    end
+  end
+end    

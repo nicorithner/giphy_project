e196f3fdf55b9c63d0b64de5ce76e45396ca2c05
Nico Rithner <nicorithner@gmail.com>
2020-10-15 11:48:20 -0600

    Update vcr cassete and search

--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 RSpec.describe "Search Facade" do
   describe "It can perform a gifs search using an api call" do
-    it "Returns a list of 10 gifs", :vcr do
-        gifs = SearchFacade.search_gifs('Bender')
+    it "Returns a list of 10 gifs" do
+      VCR.use_cassette('giphy_search') do
+        gifs = SearchFacade.search_gifs('Duffy Duck')
         
         expect(gifs).to be_an(Array)
         expect(gifs.count).to eq(10)
         expect(gifs.first).to be_a(Gif)
         expect(gifs.first.image).to be_a(String)
+      end
     end
   end
 end
